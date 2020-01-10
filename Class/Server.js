5efb47731ce54e28738930a3c6162b0691ff811e
bozoweed <bozoweed@gmail.com>
2020-01-10 17:35:24 +0100

    un petit exmple de loading

--- Class/Server.js
+++ Class/Server.js
@@ -1,19 +1,22 @@
 node_ssh = require('node-ssh')
  class Server {
 
+	c_DB_Id = 0
 	c_host = "fezf"
 	c_username = "ezfzef"
 	c_password = "ezfzef"
 	c_power = "1000"
-	c_avaiblepower = "50"
+	c_used = "50"
 	ssh = "qsdq"
 	IsNew = false;
 	IsDeprecated = false;
-	constructor(X_host, X_password, X_username, newone) {
+	constructor(DB_Id, X_host, X_password, X_username, x_power, x_Used, newone) {
        
 		this.c_host = X_host
 		this.c_password = X_password
 		this.c_username = X_username
+		this.c_power = x_power
+		this.c_used = x_Used
 		this.ConnectSsh()
 		this.IsNew = newone// ca c'est pour le loading du cache ou si on en ajoute un
 	}
--- ServerManager/ServerManagerMain.js
+++ ServerManager/ServerManagerMain.js
@@ -1,23 +1,28 @@
-require(`../Class/Server.js`);
 require(`../index.js`);
+const { Server } = require(`../Class/Server.js`);
+
 
 class ServerManagerMain 
 {
 	base ; // doit être la main classe pour avoir accer a tous les manager 
 	ServerList = []
+	ServerId= 0;
 	constructor(Base) {
        
 		this.base = Base;
 		//mysql loading to cache
-		Base.DbManager.queryAsync("SELECT * FROM `dc_comments` ",(a)=> {
+		Base.DbManager.queryAsync("SELECT * FROM `dc_comments` ",(result)=> {
 		
-		console.log(a)
+			result.foreach(object =>{
+				LoadServerToCash(object)
+			})
 		})
 	}
 
 	async LoadServerToCash(InfoDb)
-	{
-		const db = new Db()
+	{	
+		ServerId = InfoDb.Id
+		ServerList.push(new Server(InfoDb.Id, InfoDb.Host, InfoDb.Password, InfoDb.Username, InfoDb.Puissance, InfoDb.UsedPuissance, false))
 		
 	}
 
