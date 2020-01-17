6cb191fcc73330b07c2a039f0e4362c86eac18b4
bozoweed <bozoweed@gmail.com>
2020-01-17 17:07:08 +0100

    rework request manager

--- RequestManager/Request/Test/RequestTest.js
+++ RequestManager/Request/Test/RequestTest.js
@@ -1,17 +1,16 @@
 
 class RequestTest {
-   
-    constructor( RequestManager) {
-        
+
+    constructor(RequestManager) {
        RequestManager.AddEventHandler("/myApiLinkRequest", this.Test)
     }
 
-    Test(base,query, response)
+    Test(base, query, response)
     {   
        var message = ""
        console.log(query)
        base.ServerManager.AddServerToCash()//exemple de call interne api ;)
-       console.log(base.ServerManager)
+       
           if (query.message) message = query.message; else message = "not found";//exemple return way ***
             response.writeHead(200, { "Content-Type": "text/html" });//exemple return way ***
             response.write(`message ${message}.`);//exemple return way ***
--- RequestManager/RequestManagerMain.js
+++ RequestManager/RequestManagerMain.js
@@ -2,69 +2,68 @@ const url = require('url');
 const http = require('http');
 var qs = require('querystring');
 
-
 const { RequestTest } = require('./Request/Test/RequestTest.js');
 
 
 
- 
-  var  base 
-  var EventHandler = []
 class RequestManagerMain {
     
     
    // EventHandler// shoud be EventHandler["myeventName"](query)
     
-    
+      base 
+    EventHandler = []
     WebServer 
     constructor(Base, ListenPort) {
         
-        base = Base
-        this.WebServer = http.createServer(this.ExectuteEvent);// on créer le server web avec le call back pour gérer les request async !
+        this.base = Base
+        this.WebServer = http.createServer(async (request, response) => {
+             this.RunEvent(request, response)
+        });// on créer le server web avec le call back pour gérer les request anonyme (pour rester dans le main thread) async  !
         this.WebServer.listen(ListenPort);//port du server web api
         
-		new RequestTest(this)
+		new RequestTest( this)
     }
 
-    async ExectuteEvent(request, response) {
-          try {
-               var stat, query, message;
-                query = url.parse(request.url, true).query;
+    RunEvent(request, response){
+             try {              
+                   var stat, query, message;
+                    query = url.parse(request.url, true).query;
 
-               if (request.method == 'POST') {
+                   if (request.method == 'POST') {
 
-                    var body = '';
+                        var body = '';
     
-                    request.on('data', function (data) {
-                        body += data;
+                        request.on('data', function (data) {
+                            body += data;
     
-                        // Too much POST data, kill the connection!
-                        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-                        if (body.length > 1e6)
-                            request.connection.destroy();
-                    });
+                            // Too much POST data, kill the connection!
+                            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+                            if (body.length > 1e6)
+                                request.connection.destroy();
+                        });
     
-                    request.on('end', function () {
-                        var post = qs.parse(body);
-                        console.log(post['efzfez'])                       
-                    });
+                        request.on('end', function () {
+                            var post = qs.parse(body);
+                            console.log(post['efzfez'])                       
+                        });
 
-               } else { // if the request is get
+                   } else { // if the request is get
 
-                    const love = new URL(request.url, "http://" + request.headers.host)
-                   if( EventHandler[love.pathname] != null)      
-                   {
-                        EventHandler[love.pathname](base, query, response)
+                        const love = new URL(request.url, "http://" + request.headers.host)
+                       if(this.EventHandler[love.pathname] != null)      
+                       {
+                            this.EventHandler[love.pathname]( this.base, query, response)
+                       }
                    }
-               }
-          } catch (error) {
-             console.log(error)
-          }
-    }
+              } catch (error) {
+                 console.log(error)
+              }
+	}
 
     
     AddEventHandler(request, Function) {
-        EventHandler[request] = Function    
+        this.EventHandler[request] = Function    
     }
 
 }
